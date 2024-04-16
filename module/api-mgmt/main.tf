8e708a7123a147eabbbafab609fd24cf803ef56f
Joshua Glass <joshua.glass@kainos.com>
2024-04-16 16:02:37 +0100

    create stv 2 in uk south

--- module/api-mgmt/main.tf
+++ module/api-mgmt/main.tf
@@ -1,3 +1,12 @@
+resource "azurerm_public_ip" "apim" {
+  name                = "apim-ip"
+  location            = var.apim_location
+  resource_group_name = var.apim_resource_group_name
+  domain_name_label   = "apim-test-2024"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_api_management" "api_mgmt" {
   name                = var.apim_name
   location            = var.apim_location
@@ -8,13 +17,15 @@ resource "azurerm_api_management" "api_mgmt" {
 
   virtual_network_type = "Internal"
 
+  public_ip_address_id = azurerm_public_ip.apim.id
+
   virtual_network_configuration {
     subnet_id = var.apim_subnet_id
   }
 
-  zones = "TODO"
-
   identity {
     type = "SystemAssigned"
   }
+
+  depends_on = [ azurerm_public_ip.apim ]
 }
